030775d9e9ef553d3636273419a147fda677779f
Rx <ruixiangsun123@hotmail.com>
2021-01-06 19:21:53 +0100

    add build

---
+++ build/CMakeFiles/testgithublocal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testgithublocal.dir/main.cpp.o"
+  "testgithublocal"
+  "testgithublocal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testgithublocal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
